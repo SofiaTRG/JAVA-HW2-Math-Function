1965030314124ee84d7cb081f9e746d2592199fb
Sofia Torgovezky <sofiat@campus.technion.ac.il>
2023-05-28 22:25:45 +0300

    24/05: - Part A maybe finished. - Object function probably would be abstract. 27/05: - Function class fixed errors, we will override all methods in the sons classes. 28/05: - Constant class is done. - Sum class is done. - MultiSum is done. - Started the quotient class. - Started Negation class.

--- src/MultiSum.java
+++ src/MultiSum.java
@@ -1,3 +1,71 @@
 public class MultiSum extends Function {
+    private Function[] functions;
 
+    public MultiSum(Function... functions) {
+        this.functions = functions;
+    }
+
+    @Override
+    public double valueAt(double x) {
+        double sum = 0.0;
+        for (Function function : functions) {
+            sum += function.valueAt(x);
+        }
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Function function : functions) {
+            sb.append(function.toString());
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String derivative() {
+        StringBuilder sb = new StringBuilder();
+        for (Function function : functions) {
+            sb.append(function.derivative());
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public double bisectionMethod(double a, double b, double epsilon) {
+        double sum = 0.0;
+        for (Function function : functions) {
+            sum += function.bisectionMethod(a, b, epsilon);
+        }
+        return sum;
+    }
+
+    @Override
+    public double newtonRaphsonMethod(double a, double b) {
+        double sum = 0.0;
+        for (Function function : functions) {
+            sum += function.newtonRaphsonMethod(a, b);
+        }
+        return sum;
+    }
+
+    @Override
+    public double newtonRaphsonMethod(double a) {
+        double sum = 0.0;
+        for (Function function : functions) {
+            sum += function.newtonRaphsonMethod(a);
+        }
+        return sum;
+    }
+
+    @Override
+    public String taylorPolynomial(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (Function function : functions) {
+            sb.append(function.taylorPolynomial(n));
+        }
+        return sb.toString();
+    }
 }
+
---
+++ src/Negation.java
@@ -0,0 +1,3 @@
+public class Negation extends Function{
+
+}
