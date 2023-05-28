36666e68bb2bd9b93adad65001e0d04cf54eb0cb
Sofia Torgovezky <sofiat@campus.technion.ac.il>
2023-05-28 19:52:04 +0300

    24/05: - Part A maybe finished. - Object function probably would be abstract. 27/05: - Function class fixed errors, we will override all methods in the sons classes. 28/05: - Constant class is done. - Sum class is done. - Started the quotient class.

--- src/Quotient.java
+++ src/Quotient.java
@@ -2,4 +2,42 @@ public class Quotient extends Function {
     Function function1;
     Function function2;
 
+    @Override
+    public double valueAt(double x) {
+        return function1.valueAt(x)/function2.valueAt(x);
+    }
+
+    @Override
+    public String toString() {
+        return function1.toString() + function2.toString();
+    }
+
+    //NEED TO USE THE PRODUCT FUNCTION???
+    @Override
+    public String derivative() {
+        return function1.derivative();
+    }
+
+    @Override
+    public double bisectionMethod(double a, double b, double epsilon) {
+        return 0;
+    }
+
+    @Override
+    public double bisectionMethod(double a, double b, double epsilon) { return 0; }
+
+    @Override
+    public double newtonRaphsonMethod(double a, double b) {
+        return 0;
+    }
+
+    @Override
+    public double newtonRaphsonMethod(double a) {
+        return 0;
+    }
+
+    @Override
+    public String taylorPolynomial(int n) {
+        return null;
+    }
 }
